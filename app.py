70c417419706fcda3a7567088dc1adc77de3af33
maechan <miraclethankyou@gmail.com>
2019-05-01 19:32:48 +0900

    a

--- app.py
+++ app.py
@@ -96,6 +96,7 @@ def rec(data, gc):
                 worksheet.update_cell(1, 1, 0)
             elif offset >= 0:
                 flg = True
+                
             else:
                 flg = False
             if flg is True:
